3b8e6bac66441a821c820e6700fb3495efd7815d
Chris Allison <chris@sproutzo.com>
2022-12-02 16:46:20 -0500

    Friday ending point

--- script.js
+++ script.js
@@ -1,3 +1,42 @@
 const h2 = document.createElement("h2")
-h2.textContent="this is from javascript"
+h2.textContent="Intro"
 document.querySelector("body").appendChild(h2);
+
+
+
+function textAppears(){
+    var text = document.getElementById("secretText")
+    if (text.style.display ==="none") {
+        text.style.display = "block";
+    } else {
+        text.style.display = "none";
+
+}
+}
+
+function MarfredAppears(){
+    var secret = document.getElementById("secretImage")
+    if (secret.style.display ==="none") {
+        secret.style.display = "block";
+    } else {
+        secret.style.display = "none";
+}
+    var notSecret = document.getElementById("defaultImage")
+if (notSecret.style.display ==="none") {
+    notSecret.style.display = "block";
+} else {
+    notSecret.style.display = "none";
+
+}
+}
+
+
+
+//function toggleText() {
+//    var text = document.getElementById("demo");
+//    if (text.style.display === "none") {
+//        text.style.display = "block";
+//    } else {
+//        text.style.display = "none";
+//    }
+//    }
